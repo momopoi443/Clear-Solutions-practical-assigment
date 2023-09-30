516c386f7e61bca34161c1a4ddc397f620d97eb5
momopoi443 <myzika99993824@gmail.com>
2023-09-30 12:17:25 +0300

    Initial commit with dependencies

---
+++ src/test/java/com/example/clearsolutionspracticalassigment/ClearSolutionsPracticalAssigmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clearsolutionspracticalassigment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClearSolutionsPracticalAssigmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
