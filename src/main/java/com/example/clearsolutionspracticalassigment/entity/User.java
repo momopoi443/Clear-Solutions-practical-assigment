3dd7f120ff38c4629411f0a47b9039943813f4d1
momopoi443 <myzika99993824@gmail.com>
2023-09-30 13:02:30 +0300

    Implemented User entity Defined User repository contract Added needed properties for connecting to DB

---
+++ src/main/java/com/example/clearsolutionspracticalassigment/entity/User.java
@@ -0,0 +1,61 @@
+package com.example.clearsolutionspracticalassigment.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.NaturalId;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+
+    public User(UUID uuid) {
+        this.uuid = uuid;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", unique = true, nullable = false)
+    private Long id;
+
+    @NaturalId
+    @Column(name = "uuid", unique = true, nullable = false, updatable = false)
+    @Setter(value = AccessLevel.PRIVATE)
+    private UUID uuid;
+
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+
+    @Column(name = "firstName", nullable = false)
+    private String firstName;
+
+    @Column(name = "lastName", nullable = false)
+    private String lastName;
+
+    @Column(name = "birthDate", nullable = false)
+    private LocalDate birthDate;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "phoneNumber")
+    private String phoneNumber;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User teacher)) return false;
+
+        return getUuid().equals(teacher.getUuid());
+    }
+
+    @Override
+    public int hashCode() {
+        return getUuid().hashCode();
+    }
+}
